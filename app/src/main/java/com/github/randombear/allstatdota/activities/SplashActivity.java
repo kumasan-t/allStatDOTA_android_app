3c2af4f4842eea2dea6a6879aa2b1da2b37cc9c0
randomBEAR <lorenzo.travagliati@gmail.com>
2017-07-30 17:32:41 +0200

    Added new Splash Activity.

---
+++ app/src/main/java/com/github/randombear/allstatdota/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.github.randombear.allstatdota.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.randombear.allstatdota.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_layout);
+    }
+}
