734b5bd8373df8238096a3289527dc5fe33ca608
randomBEAR <lorenzo.travagliati@gmail.com>
2017-09-07 18:21:32 +0200

    Added MatchEntry in Contract.
    TODO: add PlayerEntry to contractor and define create, read and puts methods.

--- app/src/main/java/com/github/randombear/allstatdota/dataaccessobject/utility/StatContract.java
+++ app/src/main/java/com/github/randombear/allstatdota/dataaccessobject/utility/StatContract.java
@@ -18,6 +18,17 @@ public final class StatContract {
         public static final String COLUMN_NAME_NUM_RESULTS = "num_results";
         public static final String COLUMN_NAME_TOTAL_RESULTS = "total_results";
         public static final String COLUMN_NAME_RESULTS_REMAINING = "results_remaining";
-        public static final String COLUMN_NAME_MATCHES = "matches";
+
+    }
+
+    public static class MatchEntry  implements BaseColumns {
+        public static final String TABLE_NAME = "match";
+        public static final String COLUMN_NAME_MATCH_ID = "match_id";
+        public static final String COLUMN_NAME_MATCH_SEQ_NUM = "match_se_num";
+        public static final String COLUMN_NAME_START_TIME = "start_time";
+        public static final String COLUMN_NAME_LOBBY_TYPE = "lobby_type";
+        public static final String COLUMN_NAME_RADIANT_TEAM_ID = "radiant_team_id";
+        public static final String COLUMN_NAME_DIRE_TEAM_ID = "dire_team_id";
+        public static final String COLUMN_NAME_MATCH_HISTORY = "match_history_id";
     }
 }
