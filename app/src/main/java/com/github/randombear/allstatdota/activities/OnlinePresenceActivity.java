d1cbd482cc1060319c68a9ad3f285280d23899b1
randomBEAR <lorenzo.travagliati@gmail.com>
2017-11-03 11:35:30 +0100

    Added connection indicator and updated fab icon

--- app/src/main/java/com/github/randombear/allstatdota/activities/OnlinePresenceActivity.java
+++ app/src/main/java/com/github/randombear/allstatdota/activities/OnlinePresenceActivity.java
@@ -3,22 +3,19 @@ package com.github.randombear.allstatdota.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.github.randombear.allstatdota.R;
 import com.github.randombear.allstatdota.dataaccessobject.entities.User;
 import com.google.android.gms.auth.api.Auth;
@@ -34,14 +31,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Map;
 
 public class OnlinePresenceActivity extends AppCompatActivity {
 
@@ -211,8 +203,14 @@ public class OnlinePresenceActivity extends AppCompatActivity {
             protected void populateView(final View v, User model, int position) {
                 TextView name = v.findViewById(R.id.user_single_name);
                 TextView online = v.findViewById(R.id.user_single_online_status);
+                ImageView indicator = v.findViewById(R.id.user_single_layout_indicator);
                 name.setText(model.getName());
                 online.setText(model.isOnline() ? "connected" : "last seen: " + model.getDate());
+                if (model.isOnline()) {
+                    indicator.setVisibility(View.VISIBLE);
+                } else {
+                    indicator.setVisibility(View.INVISIBLE);
+                }
             }
         };
         mListView.setAdapter(mAdapter);
